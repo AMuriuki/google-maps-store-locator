e67339c7aafb7b302514a65cc31683e7e90459be
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-09-13 11:04:01 +0300

    Hello, world! with a map

---
+++ app.js
@@ -0,0 +1,7 @@
+function initMap() {
+    // create the map
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: { lat: -1.274610, lng: 36.875759 }
+    });
+}
